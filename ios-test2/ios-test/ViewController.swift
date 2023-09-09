17deeb2102a9c75fb51625640395980f53c12c99
Miguel Mendoza <cmendoza@monex.com.mx>
2023-09-09 08:37:15 -0600

    test

---
+++ ios-test2/ios-test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-test
+//
+//  Created by Miguel Mendoza on 09/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
